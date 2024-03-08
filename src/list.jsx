c04307c2c06d0bd73edf521def58306fbdf17f9f
shahinexy <shahinexy@gmail.com>
2024-03-09 00:04:49 +0600

    different way conditoinal rendaring

--- src/list.jsx
+++ src/list.jsx
@@ -1,4 +1,4 @@
-// export default function List({task}){
+// export default function List({task, isDone}){
 //     return(
 //         <div>
 //             <li>Task: {task}</li>
@@ -6,12 +6,41 @@
 //     )
 // }
 
+// conditional rendaring: 01.
+// export default function List({ task, isDone }) {
+//     if(isDone){
+//         return <li>Finish: {task}</li>
+//     }
+//     else{
+//         return <li>Working on: {task}</li>
+//     }
+// }
 
-export default function List({ task, isDone }) {
-    if(isDone){
-        return <li>Finish: {task}</li>
-    }
-    else{
-        return <li>Working on: {task}</li>
-    }
+
+// Conditional rendaring: 02.   (tarnary operatore.)
+// export default function List({task, isDone}){
+//     return(
+//         <div>
+//             <li>{isDone ? 'Finished' : 'Working'} {task}</li>
+//         </div>
+//     )
+// }
+
+
+// Conditional rendaring: 02.   (&&)
+// export default function List({task, isDone}){
+//     return(
+//         <div>
+//             <li>{task} {isDone && ': Done'}</li>
+//         </div>
+//     )
+// }
+
+// Conditional rendaring: 02.   (||)
+export default function List({task, isDone}){
+    return(
+        <div>
+            <li>{task} {isDone || ': Not yet'}</li>
+        </div>
+    )
 }
