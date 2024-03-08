fb88091f5fcf2ddcb3e53208e04a33af4ce1f459
shahinexy <shahinexy@gmail.com>
2024-03-08 23:29:50 +0600

    another way to add props

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,10 @@ function App() {
       <StudentDetails name="Shifat" roll="02"></StudentDetails>
       <StudentDetails name="Rifat" roll="03"></StudentDetails>
       <StudentDetails name="Khalil" roll="04"></StudentDetails>
+      <ExamDetails stclass="5" stroll="01"></ExamDetails>
+      <ExamDetails stclass="5" stroll="02"></ExamDetails>
+      <ExamDetails stclass="6" stroll="01"></ExamDetails>
+      <ExamDetails></ExamDetails>
     </>
   )
 }
@@ -61,6 +65,17 @@ function StudentDetails(props){
   return(
     <div className='stdetails'>
       <h2>Student Name: {props.name}. Student Roll: {props.roll}</h2>
+    </div>
+  )
+}
+
+// another way to use props
+// we can also add default value
+function ExamDetails({stclass='A', stroll=0}){
+  return(
+    <div className="stdetails">
+      <h2>Class: {stclass}</h2>
+      <h2>Roll: {stroll}</h2>
     </div>
   )
 }
